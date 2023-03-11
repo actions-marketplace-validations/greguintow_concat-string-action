fa75c3ec581d64e0f63fcb210174464e472eba55
Greg Teixeira <greg.teixeira123@gmail.com>
2023-03-11 19:56:43 -0300

    feat: allow to add default values

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const core = require('@actions/core')
 
 try {
-	const re = /{([^}]+){1,}}/
-	const globalRe = new RegExp(re, 'g')
-	const string = core.getInput('string')
-	const ids = [...string.matchAll(globalRe)].map(match => match.slice(0, 2))
-	let finalString = string
-	ids.forEach(([toReplace, id]) => {
-		const mappedId = process.env[id]
-		finalString = finalString.replace(toReplace, mappedId || id)
-	})
+  const re = /{([^}]+){1,}}/
+  const globalRe = new RegExp(re, 'g')
+  const string = core.getInput('string')
+  const ids = [...string.matchAll(globalRe)].map(match => match.slice(0, 2))
+  let finalString = string
+  ids.forEach(([toReplace, id]) => {
+    const mappedId = process.env[id] || process.env.default
+    finalString = finalString.replace(toReplace, mappedId || id)
+  })
 
-	core.setOutput('replaced', finalString)
+  core.setOutput('replaced', finalString)
 } catch (error) {
-	core.setFailed(error.message)
+  core.setFailed(error.message)
 }
